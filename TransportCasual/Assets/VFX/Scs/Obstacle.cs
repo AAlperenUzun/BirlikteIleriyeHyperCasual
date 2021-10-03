db437d9c70818267e749f0cad094125be1ff67e4
Evaluator <thrvoid97@gmail.com>
2021-10-03 13:58:43 +0300

    Traffic Jam Added

--- TransportCasual/Assets/VFX/Scs/Obstacle.cs
+++ TransportCasual/Assets/VFX/Scs/Obstacle.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public int Amount;
+    public int Amount;    
+    [Header("Set if moving object")]
+    public bool movingObstacle;
+    public int moveSpeed;
+
+    private bool started;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        EventManager.StartListening(Events.StartTap, onScreenTapped);
+    }
     private void onCollision()
     {
         var eventParam = new EventParam { intParam = Amount };
         EventManager.TriggerEvent(Events.MoneyCollect, eventParam);
-        //Collision particle effect
+    }
+
+    private void onScreenTapped(EventParam param)
+    {
+        started = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         onCollision();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movingObstacle && started)
+        {
+            rb.velocity = transform.right * moveSpeed;
+        }
     }
 }
