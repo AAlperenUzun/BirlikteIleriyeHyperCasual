e07a5fe7fdfee1fcad5d027c4c4e16ff1bf92424
Evaluator <thrvoid97@gmail.com>
2021-10-03 17:34:17 +0300

    Obstacle fly added

--- TransportCasual/Assets/VFX/Scs/Obstacle.cs
+++ TransportCasual/Assets/VFX/Scs/Obstacle.cs
@@ -18,11 +18,15 @@ public class Obstacle : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         EventManager.StartListening(Events.StartTap, onScreenTapped);
     }
-    private void onCollision()
+    private void onCollision(Collider other)
     {
         var eventParam = new EventParam { intParam = Amount };
         EventManager.TriggerEvent(Events.MoneyCollect, eventParam);
+
         CameraControl.instance.ShakeCamera(20f, .2f);
+        rb.useGravity = true;
+        rb.AddExplosionForce(500f, ((other.transform.position + transform.position) / 2) + new Vector3(0,-2f,0), 20f);
+        
     }
 
     private void onScreenTapped(EventParam param)
@@ -32,7 +36,7 @@ public class Obstacle : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        onCollision();
+        onCollision(other);
     }
 
     private void FixedUpdate()
